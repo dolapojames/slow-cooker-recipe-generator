4c231a99a86d6f26ddd92cbac729f70c209807e1
dolapojames <oludolapo@hotmail.com>
2025-02-22 10:34:59 +0000

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+function displayRecipe(response) {
+  console.log("generating recipe");
+
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: ["Hello World"],
-    autoStart: true,
-    cursor: "",
-  });
+  let ingredients = document.querySelector("#ingredients");
+  let apiKey = "5ca9a4e04df3dddde0tdc3bec6cd3f5o";
+  let prompt = `What is a tasty slow cooker recipe with ${ingredients.value}`;
+  let context =
+    "Please show recipe in HTML form. Do not include an introdcution. Do not write html at the top";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeElement = document.querySelector("#recipe-generator");
