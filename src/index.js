e46c50dca6ba9f14b5403ee444f52aff147e41ff
dolapojames <oludolapo@hotmail.com>
2025-02-24 10:12:35 +0000

    Modified HTML and JavaScript to improve UX

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ function generateRecipe(event) {
   let apiKey = "5ca9a4e04df3dddde0tdc3bec6cd3f5o";
   let prompt = `What is a tasty slow cooker recipe with ${ingredients.value}`;
   let context =
-    "Please show recipe in HTML form. Do not include an introdcution. Do not write html at the top";
+    "Please show recipe in HTML. Do not include an introdcution. Do not write html at the top.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `Generation a ${ingredients.value} recipe for you`;
 
   console.log("generating recipe");
   console.log(`Prompt: ${prompt}`);
